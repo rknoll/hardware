432d630980cc927d7fcbdd4937574d4049392ee0
Richard Knoll <richard@rknoll.at>
2014-07-30 14:35:04 +0200

    fixed copy from resources

--- src/main/groovy/at/rknoll/gradle/hardware/pshdl/PshdlPrepareCompileTask.groovy
+++ src/main/groovy/at/rknoll/gradle/hardware/pshdl/PshdlPrepareCompileTask.groovy
@@ -3,6 +3,7 @@ package at.rknoll.gradle.hardware.pshdl
 import org.gradle.api.file.FileTree
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
+import java.nio.file.Files
 
 class PshdlPrepareCompileTask extends SourceTask {
     @TaskAction
@@ -14,10 +15,9 @@ class PshdlPrepareCompileTask extends SourceTask {
 		destDir.mkdirs()
 
         FileTree sources = getSource()
-		URL url = this.getClass().getResource("/at/rknoll/gradle/hardware/pshdl/pshdl_pkg.vhd");
-		File pshdlPkgResource = new File(url.getFile());
-		File pshdlPkg = new File(destDir, "pshdl_pkg.vhd");
-		pshdlPkg.bytes = pshdlPkgResource.bytes
+		InputStream resourceStream = this.getClass().getResourceAsStream("/at/rknoll/gradle/hardware/pshdl/pshdl_pkg.vhd")
+		File pshdlPkg = new File(destDir, "pshdl_pkg.vhd")
+		Files.copy(resourceStream, pshdlPkg.getAbsoluteFile().toPath())
 		
 		project.hardwareSources.addVertex(pshdlPkg)
 
