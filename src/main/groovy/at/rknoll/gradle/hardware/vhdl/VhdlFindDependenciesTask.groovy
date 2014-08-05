ea88119ebb27bf0f2a22656aaeb2e64c5414377e
Richard Knoll <richard@rknoll.at>
2014-08-05 13:26:21 +0200

    find correct dependencies for vhdl units

--- src/main/groovy/at/rknoll/gradle/hardware/vhdl/VhdlFindDependenciesTask.groovy
+++ src/main/groovy/at/rknoll/gradle/hardware/vhdl/VhdlFindDependenciesTask.groovy
@@ -6,6 +6,7 @@ import org.gradle.api.tasks.TaskAction
 import org.jgrapht.alg.CycleDetector
 import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.traverse.TopologicalOrderIterator
+import java.util.regex.Matcher
 
 class VhdlFindDependenciesTask extends SourceTask {
 
@@ -13,7 +14,7 @@ class VhdlFindDependenciesTask extends SourceTask {
     def compile() {
         FileTree sources = getSource()
 
-		// depend on all external dependencies
+		// depend on all external dependencies by default
 		for (File file : project.hardwareSources.vertexSet()) {
 			if (!sources.contains(file)) {
 				sources.visit { src ->
@@ -22,10 +23,30 @@ class VhdlFindDependenciesTask extends SourceTask {
 			}
 		}
 
-		// TODO: find internal dependencies for this file
-        /*sources.visit { file ->
+		def dependsOn = [:]
+		def definesUnits = [:]
 
-        }*/
+		for (File file : project.hardwareSources.vertexSet()) {
+			String fileContents = file.text
+			dependsOn[file] = []
+			definesUnits[file] = []
+			fileContents.replace('\r','').tokenize('\n').each {
+				Matcher matcher = (it =~ /^[\s\t]*(?:(?:[^-].*)|(?:-[^-]+.*)|(?:-$))work\.([^\.\s\t\(]+).*/);
+				if (matcher.matches()) dependsOn[file].add(matcher[0][1].toLowerCase())
+				matcher = (it =~ /(?:(?:^(?:(?:entity)|(?:package)))|(?:^[\s\t]*(?:(?:[^-].*)|(?:-[^-]+.*)|(?:-$))(?:(?:entity)|(?:package))))[\s\t]+([^\s\t]+)[\s\t]+is.*/);
+				if (matcher.matches()) definesUnits[file].add(matcher[0][1].toLowerCase())
+			}
+		}
+
+		for (File file : project.hardwareSources.vertexSet()) {
+			dependsOn[file].each { depId ->
+				definesUnits.each { defFile, unitList ->
+					if (unitList.contains(depId)) {
+						project.hardwareSources.addEdge(defFile, file);
+					}
+				}
+			}
+		}
     }
 
 }
